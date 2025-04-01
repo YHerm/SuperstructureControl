f9e1c901f2d4feded8eb94384ebe001eba368458
YHerm <0549405422.yh@gmail.com>
2025-04-01 18:16:29 +0300

    format

--- src/main/java/frc/robot/visualizers/FollowPathCommand.java
+++ src/main/java/frc/robot/visualizers/FollowPathCommand.java
@@ -8,37 +8,37 @@ import java.util.function.Consumer;
 
 public class FollowPathCommand extends Command {
 
-    private final Consumer<Translation2d> setPosition;
-    private final double delayBetweenPoints;
-    private final Translation2d[] points;
+	private final Consumer<Translation2d> setPosition;
+	private final double delayBetweenPoints;
+	private final Translation2d[] points;
 
-    private double setPositionTime = 0;
-    private int currentPoint = 1;
+	private double setPositionTime = 0;
+	private int currentPoint = 1;
 
-    public FollowPathCommand(Consumer<Translation2d> setPosition, double delayBetweenPoints, Translation2d... points) {
-        this.setPosition = setPosition;
-        this.delayBetweenPoints = delayBetweenPoints;
-        this.points = points;
-    }
+	public FollowPathCommand(Consumer<Translation2d> setPosition, double delayBetweenPoints, Translation2d... points) {
+		this.setPosition = setPosition;
+		this.delayBetweenPoints = delayBetweenPoints;
+		this.points = points;
+	}
 
-    @Override
-    public void initialize() {
-        setPositionTime = TimeUtil.getCurrentTimeSeconds();
-        currentPoint = 1;
-    }
+	@Override
+	public void initialize() {
+		setPositionTime = TimeUtil.getCurrentTimeSeconds();
+		currentPoint = 1;
+	}
 
-    @Override
-    public void execute() {
-       if (TimeUtil.getCurrentTimeSeconds() - setPositionTime >= delayBetweenPoints) {
-           setPosition.accept(points[currentPoint]);
-           currentPoint++;
-           setPositionTime = TimeUtil.getCurrentTimeSeconds();
-       }
-    }
+	@Override
+	public void execute() {
+		if (TimeUtil.getCurrentTimeSeconds() - setPositionTime >= delayBetweenPoints) {
+			setPosition.accept(points[currentPoint]);
+			currentPoint++;
+			setPositionTime = TimeUtil.getCurrentTimeSeconds();
+		}
+	}
 
-    @Override
-    public boolean isFinished() {
-        return currentPoint >= points.length;
-    }
+	@Override
+	public boolean isFinished() {
+		return currentPoint >= points.length;
+	}
 
 }
